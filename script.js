1175141a4e0d205fdf300c17a27b3429f745d311
jaydeexsf <140901459+jaydeexsf@users.noreply.github.com>
2024-08-12 20:40:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.category-btn');
+    const serviceItems = document.querySelectorAll('.service-item');
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        const category = button.getAttribute('data-category');
+  
+        serviceItems.forEach(item => {
+          if (category === 'all' || item.getAttribute('data-category') === category) {
+            item.classList.remove('hidden');
+          } else {
+            item.classList.add('hidden');
+          }
+        });
+      });
+    });
+  });
+  
