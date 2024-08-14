350b124e94b28e27396494cc917bb1a3e3f7384b
johannes <jaydeexsf@gmail.com>
2024-08-14 23:02:23 +0200

    data

--- script.js
+++ script.js
@@ -3,22 +3,22 @@
 // import sercviceData from "./database";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.category-btn');
-    const serviceItems = document.querySelectorAll('.service-item');
+    // const buttons = document.querySelectorAll('.category-btn');
+    // const serviceItems = document.querySelectorAll('.service-item');
   
-    buttons.forEach(button => {
-      button.addEventListener('click', () => {
-        const category = button.getAttribute('data-category');
+    // buttons.forEach(button => {
+    //   button.addEventListener('click', () => {
+    //     const category = button.getAttribute('data-category');
   
-        serviceItems.forEach(item => {
-          if (category === 'all' || item.getAttribute('data-category') === category) {
-            item.classList.remove('hidden');
-          } else {
-            item.classList.add('hidden');
-          }
-        });
-      });
-    });
+    //     serviceItems.forEach(item => {
+    //       if (category === 'all' || item.getAttribute('data-category') === category) {
+    //         item.classList.remove('hidden');
+    //       } else {
+    //         item.classList.add('hidden');
+    //       }
+    //     });
+    //   });
+    // });
     
     // const divServiceItem = document.createElement('div');
     // const imgServiceItem = document.createElement('img');
